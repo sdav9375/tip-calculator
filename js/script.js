eb382bc259e1900406b76cbf6ed6ad7195dc78a2
Susan Davies <sdav9375@gmail.com>
2017-01-07 15:04:21 -0500

    validation and styling

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
 function calculateTip() {
-  var bill = document.getElementById('totalBill').value;
-  var percent = document.getElementById('percentTip').value;
-  var people = document.getElementById('people').value;
 
-  var totalTip = bill * percent / people;
+  var bill = totalBill.value;
+  var percent = percentTip.value;
 
-document.getElementById('tip').innerHTML = totalTip;
-document.getElementById('totalTip').style.display = "initial";
-}
+  if (people.value === "" || people.value <= 1) {
+    people.value = "1";
+    document.getElementById('each').style.display = "none";
+  } else {
+    document.getElementById('each').style.display = "block";
+  }
 
+  var ppl = people.value;
+
+  var totalTip = (bill * percent) / ppl;
+  totalTip = Math.round(totalTip * 100) / 100;
+  totalTip = totalTip.toFixed(2);
+
+  document.getElementById('tip').innerHTML = totalTip;
+  document.getElementById('totalTip').style.display = "initial";
+};
 
 
 
